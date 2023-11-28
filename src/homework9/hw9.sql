8fe627a90ce6c35aab293b50351239032807183d
elizavetabadger <Artbel543@yandex.ru>
2023-11-28 10:45:34 +0300

    hw9

---
+++ src/homework9/hw9.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `Cloud` DROP FOREIGN KEY `Cloud_fk0`;
+
+ALTER TABLE `Cloud` DROP FOREIGN KEY `Cloud_fk1`;
+
+ALTER TABLE `Cloud` DROP FOREIGN KEY `Cloud_fk2`;
+
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_fk0`;
+
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_fk1`;
+
