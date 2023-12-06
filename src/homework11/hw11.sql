310b0c764960c27b2f8bb8adda70da7ade35d741
elizavetabadger <Artbel543@yandex.ru>
2023-12-06 14:16:39 +0300

    hw11

---
+++ src/homework11/hw11.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `Group_Admin` DROP FOREIGN KEY `Group_Admin_fk0`;
+
+ALTER TABLE `Robotic_vacuum_cleaner` DROP FOREIGN KEY `Robotic_vacuum_cleaner_fk0`;
+
+ALTER TABLE `Setting_robot` DROP FOREIGN KEY `Setting_robot_fk0`;
+
+ALTER TABLE `Setting_robot` DROP FOREIGN KEY `Setting_robot_fk1`;
+
+ALTER TABLE `Report_errors` DROP FOREIGN KEY `Report_errors_fk0`;
+
+ALTER TABLE `Token` DROP FOREIGN KEY `Token_fk0`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk0`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk1`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk2`;
+
+ALTER TABLE `Indicators_robot` DROP FOREIGN KEY `Indicators_robot_fk0`;
+
